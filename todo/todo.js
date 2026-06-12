fd1007694db49b93bd7b535749b8b6d2d56594ec
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-06-12 10:45:53 +0530

    add todo code

--- todo/todo.js
+++ todo/todo.js
@@ -1,6 +1,31 @@
 const fs = require("fs");
 const filePath = "./tasks.json";
 
+const loadTask = () =>{
+    try {
+        const dataBuffer = fs.readFileSync(filePath)
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (errro) {
+        return []
+    }
+};
+
+const saveTasks = (tasks) =>{
+ const dataJSON = JSON.stringify(tasks)
+ fs.writeFileSync(filePath, dataJSON)
+};
+
+const addTask = (task)=>{
+    const tasks = loadTask()
+    tasks.push({task})
+    saveTasks(tasks)
+    console.log("Task added", task)
+}
+
+const command = process.argv[2]
+const argument = process.argv[3]
+
 if(command === "add"){
     addTask(argument);
 }else if(command === "list"){
